6d5fb7b84fbbef0fb7fb1f5db53fd645dc97a3ff
k.jedra <kozukj@gmail.com>
2022-04-06 10:23:35 +0200

    .csv conversion to df

--- src/main/scala/Wed.scala
+++ src/main/scala/Wed.scala
@@ -1,8 +1,22 @@
 
-
-
-
 object Wed extends App {
 
-  // Morning E
+  import org.apache.spark.sql.SparkSession
+  val spark = SparkSession
+    .builder()
+    .appName("Spark SQL basic example")
+    .config("spark.some.config.option", "some-value")
+    .master("local[*]")
+    .getOrCreate()
+  // Morning Exercise
+  // DataFrame (Dataset)
+  // 1. Load CSV file
+  // 2. Dataset.withColumn + functions object
+  //  - https://spark.apache.org/docs/latest/api/scala/org/apache/spark/sql/functions$.html
+  //  - Values should all be UPPER case
+  // 3. DataFrame.show
+  import org.apache.spark.sql.functions._
+  val df = spark.read.option("delimiter", ",").option("header", "true").csv("sample2.csv")
+  val df1 = df.withColumn("Name", upper(df("Name")))
+ df1.show
 }
