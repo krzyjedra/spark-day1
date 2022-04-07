ae5871a3581dc8f50a8784072561883d9e1df966
k.jedra <kozukj@gmail.com>
2022-04-07 13:07:37 +0200

    Ex2 created

---
+++ src/main/scala/SparkEx1JL.scala
@@ -0,0 +1,28 @@
+import org.apache.spark.sql.functions.{concat, expr, lit, struct}
+
+object SparkEx1JL extends App {
+
+  import org.apache.spark.sql.SparkSession
+
+  val spark = SparkSession
+    .builder
+    .master("local[*]")
+    .getOrCreate()
+
+  import spark.implicits._
+
+  val dept = Seq(
+    ("50000.0#0#0#", "#"),
+    ("0@1000.0@", "@"),
+    ("1$", "$"),
+    ("1000.00^Test_string", "^")).toDF("VALUES", "Delimiter")
+
+  //    dept.withColumn("Delimiter",concat(lit("\\"),$"Delimiter"))
+  //    .withColumn("split_values",expr("split(VALUES, Delimiter)"))
+  //    .show(false)
+
+  dept
+    .withColumn("split_values", struct("VALUES", "Delimiter"))
+    .withColumn("split_values", expr("""split(VALUES, concat("\\", Delimiter))"""))
+    .show(false)
+}
--- src/main/scala/Thu.scala
+++ src/main/scala/Thu.scala
@@ -1,17 +1,24 @@
-import org.apache.spark.sql.SparkSession
-
 object Thu extends App {
+
+  import org.apache.spark.sql.SparkSession
+
   val spark = SparkSession
-    .builder()
-    .appName("Spark SQL basic example")
-    .config("spark.some.config.option", "some-value")
+    .builder
     .master("local[*]")
     .getOrCreate()
 
-  spark.read.format("text").load("README.md")
+  //  spark.read.format("text").load("README.md")
+  //  spark.read.text("README.md")
+  //  val ds = spark.read.text("README.md")
 
-  spark.read.text("README.md")
+  import spark.implicits._
 
-  val ds = spark.read.text("README.md")
-    :type ds
+  (0 to 5).toDF("cokolwiek")
+
+  val numbers = Seq(Seq(1, 2, 3)).toDF("numbers")
+
+  numbers.flatMap { r =>
+    val ns = r.getSeq[Int](0)
+    ns.map(n => (ns, n))
+  }.toDF("nums", "num").show(false)
 }
